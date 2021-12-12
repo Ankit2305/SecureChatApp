542dd59a3101d23add5f47e565cf6e30af2fee89
vickyy044 <43821289+vickyy044@users.noreply.github.com>
2021-12-12 13:28:00 +0530

    added firebase dependencies for authentication and database added message types (auth, message)

--- app/src/main/java/com/ankitkumar/securechatapplication/util/Constants.kt
+++ app/src/main/java/com/ankitkumar/securechatapplication/util/Constants.kt
@@ -1,7 +1,13 @@
 package com.ankitkumar.securechatapplication.util
 
-const val WEB_SOCKET_URL = "ws://192.168.2.171:3000"
+const val WEB_SOCKET_URL = "wss://secure-chat-application.herokuapp.com/"
+const val AUTH_CODE = "authCode"
+//
+//object WebSocketConnection {
+//    const val NEW_CONNECTION_REQUEST = 1000
+//}
 
-object WebSocketConnection {
-    const val NEW_CONNECTION_REQUEST = 1000
+object MessageType{
+    const val MESSAGE = 2000
+    const val AUTH = 1000
 }
